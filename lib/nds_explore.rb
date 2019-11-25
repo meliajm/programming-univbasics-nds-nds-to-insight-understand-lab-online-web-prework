0a81a15a6c766b962297e2ef46fb1908a4829822
melia miller <meliajm@gmail.com>
2019-11-25 03:34:14 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -13,6 +13,7 @@ def print_first_directors_movie_titles
   row_index = 0 
   while row_index < directors_database.length do
     puts "Row #{row_index}, column: #{directors_database[row_index]}"
+  row_index += 1
   end 
-  row_index += 1 
+   
 end
