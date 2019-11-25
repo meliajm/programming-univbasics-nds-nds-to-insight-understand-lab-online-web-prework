3a56b739e92b6ac01660619ced1182d9fd4cd6c4
melia miller <meliajm@gmail.com>
2019-11-25 03:50:34 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -23,7 +23,7 @@ def print_first_directors_movie_titles
   
   row_index = 0
   while row_index < directors_database.length do
-    puts "Row #{row_index} has #{vm[row_index]} columns"
+    puts "Row #{row_index} has #{directors_database[row_index]} columns"
  
     column_index = 0
     while column_index < directors_database[row_index].length do
