3cf37a4057999b219242646752a39ceac8ecbd90
melia miller <meliajm@gmail.com>
2019-11-25 03:20:09 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,5 +8,5 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  pretty_print_nds(directors_database)[:name][:movies]
+  print pretty_print_nds(directors_database)[:name][:movies]
 end
